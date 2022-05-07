cadb4504b1e104d977281a586d2bf30cf490a036
shibez0112 <ginta2888@gmail.com>
2022-05-07 22:40:07 +0700

    alobayad

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -3,18 +3,12 @@ class greek
 {
     public static void Main()
     {
+        Console.WriteLine("133333"); Console.WriteLine("133333"); Console.WriteLine("133333"); Console.WriteLine("133333"); Console.WriteLine("133333"); Console.WriteLine("133333"); Console.WriteLine("133333");
+        Console.WriteLine("133333");
+        Console.WriteLine("133333");
         string str = "GeeksForGeeks";
-<<<<<<< HEAD
-
-        Console.WriteLine("I'm testing this shit");
-        Console.WriteLine("Sao ko commit dc nhi");
-=======
-        char[] dest = new char[15];
-        str.CopyTo(5, dest, 0, 3);
-        Console.WriteLine(dest);
         Console.WriteLine("1");
         Console.WriteLine("133333");
 
->>>>>>> 475d4d1bd018446a0b38af5acfb7bc8da1eab17e
     }
 }
