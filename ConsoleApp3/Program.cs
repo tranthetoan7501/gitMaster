6de4265e006f63568ac953b57234dc3f268b1824
shibez0112 <ginta2888@gmail.com>
2022-05-07 22:36:53 +0700

    test merge

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,9 +4,7 @@ class greek
     public static void Main()
     {
         string str = "GeeksForGeeks";
-        char[] dest = new char[15];
-        str.CopyTo(5, dest, 0, 4);
-        Console.WriteLine("1");
+
         Console.WriteLine("I'm testing this shit");
         Console.WriteLine("Sao ko commit dc nhi");
     }
