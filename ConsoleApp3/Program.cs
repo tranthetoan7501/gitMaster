22af5a508e3c55c25b649fd791e2398255f0defd
tranthetoan7501 <68367691+tranthetoan7501@users.noreply.github.com>
2022-05-07 22:45:36 +0700

    dfdfd

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,12 +4,15 @@ class greek
     public static void Main()
     {
         string str = "GeeksForGeeks";
+        Console.WriteLine("kkkkkkas1");
         char[] dest = new char[15];
         str.CopyTo(5, dest, 0, 3);
         Console.WriteLine(dest);
-        Console.WriteLine("1");
+        
+        Console.WriteLine("âsasaas1");
+        Console.WriteLine("âsdsas1");
+        Console.WriteLine("kkkkkkas1");
 
-        Console.WriteLine("1333đfsdfgs33");
 
     }
 }
