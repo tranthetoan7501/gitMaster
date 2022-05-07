362e9d83f8d07975d3deabb7f0fdb6b1e8023b2a
shibez0112 <ginta2888@gmail.com>
2022-05-07 22:00:22 +0700

    this is new commit test

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -5,8 +5,9 @@ class greek
     {
         string str = "GeeksForGeeks";
         char[] dest = new char[15];
-        str.CopyTo(5, dest, 0, 3);
-        Console.WriteLine(dest);
+        str.CopyTo(5, dest, 0, 4);
         Console.WriteLine("1");
+        Console.WriteLine("I'm testing this shit");
+        Console.WriteLine("Sao ko commit dc nhi");
     }
 }
