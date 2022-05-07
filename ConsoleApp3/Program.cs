441dc0310b05640efe1cb38a36f776e0c6303b67
tranthetoan7501 <68367691+tranthetoan7501@users.noreply.github.com>
2022-05-07 21:14:27 +0700

    first commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class greek
+{
+    public static void Main()
+    {
+        string str = "GeeksForGeeks";
+        char[] dest = new char[15];
+        str.CopyTo(5, dest, 0, 3);
+        Console.WriteLine(dest);
+        Console.WriteLine("1");
+    }
+}
