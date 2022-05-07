66d8a0e11691b751065f9a0a27f46fe3a620ce24
shibez0112 <ginta2888@gmail.com>
2022-05-07 22:43:50 +0700

    alo1

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,9 +4,11 @@ class greek
     public static void Main()
     {
         string str = "GeeksForGeeks";
-        char[] dest = new char[15];
-        str.CopyTo(5, dest, 0, 3);
-        Console.WriteLine(dest);
+        Console.WriteLine("test commit asdnlasdlfk");
+        Console.WriteLine("test commit asdnlasdlfk");
+        Console.WriteLine("test commit asdnlasdlfk");
+        Console.WriteLine("test commit asdnlasdlfk");
+        Console.WriteLine("test commit asdnlasdlfk");
         Console.WriteLine("1");
 
         Console.WriteLine("1333đfsdfgs33");
