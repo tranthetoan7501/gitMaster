df864097f0c0922f57753639eb0b42fa0213f55d
tranthetoan7501 <68367691+tranthetoan7501@users.noreply.github.com>
2022-05-08 10:40:53 +0700

    new-branch

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,14 +8,12 @@ class greek
         char[] dest = new char[15];
         str.CopyTo(5, dest, 0, 3);
         Console.WriteLine(dest);
-        Console.WriteLine("test commit asdnlasdlfk");
-        Console.WriteLine("test commit asdnlasdlfk");
-        Console.WriteLine("test commit asdnlasdlfk");
+       
         Console.WriteLine("test commit asdnlasdlfk");
         Console.WriteLine("test commit asdnlasdlfk");
         
         Console.WriteLine("âsasaas1");
-        Console.WriteLine("âsdsas1");
+        Console.WriteLine("new branch");
         Console.WriteLine("kkkkkkas1");
 
 
